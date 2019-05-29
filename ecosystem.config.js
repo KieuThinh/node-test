be92041c1c9abc874f62d511431b024b6d9347c7
knthinh <kieuthinhpg@gmail.com>
2019-05-29 11:36:12 +0700

    init my first node-project

---
+++ ecosystem.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  apps : [
+  {
+    name: 'node-red',
+    script: '/home/knthinh//.nvm/versions/node/v11.14.0/bin/node-red',
+    args: '--userDir node-red/',
+    instances: 1,
+    autorestart: true,
+    watch: false
+  },
+  {
+    name: 'node2-test',
+    script: '/home/knthinh//.nvm/versions/node/v11.14.0/bin/node-red',
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false
+  },
+  {
+    name: 'zookeeper',
+    script: '/home/knthinh/tools/kafka/kafka_2.12-2.2.0/bin/zookeeper-server-start.sh',
+    args: '/home/knthinh/tools/kafka/kafka_2.12-2.2.0/config/zookeeper.properties',
+    instances: 1,
+    autorestart: true,
+    watch: false
+  },
+  {
+    name: 'kafka server',
+    script: '/home/knthinh/tools/kafka/kafka_2.12-2.2.0/bin/kafka-server-start.sh',
+    args: '/home/knthinh/tools/kafka/kafka_2.12-2.2.0/config/server.properties',
+    instances: 1,
+    autorestart: true,
+    watch: false
+  },
+],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
